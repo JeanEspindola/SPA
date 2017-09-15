b4bce9bc7cd9abe7b3c62fbf8f7bac6d1b898e3c
Jean Espindola <jean.espindola@adp.com>
2017-09-15 18:30:08 -0300

    Improving specs.

--- app/core/js/data-services/year-race-list-dataservice-factory-spec.js
+++ app/core/js/data-services/year-race-list-dataservice-factory-spec.js
@@ -2,18 +2,22 @@
 ngDescribe({
     name: 'year-race-list-dataservice-factory-spec.js',
     modules: [
-        'mobiquity.core'
+        'mobiquity.core',
+        'mock/yearRaceResults.json'
     ],
     inject: [
         'mobiquity.core.yearRacesListDataservice',
         '$httpBackend',
         '$q',
-        'mockedData'
+        'mockedData',
+        'mockYearRaceResults'
     ],
     mocks: {
         'mobiquity.core': {
             'mockedData': {
-                regexp: {},
+                regexp: {
+                    'yearsResult': /^\/ergast.com\/api\/f1\/2015\/results\/1.json\?.*/
+                },
                 response: {
                     'error': {applicationCode: {code: 'GENERIC_ERROR', typeCode: 'error'}}
                 },
@@ -29,8 +33,34 @@ ngDescribe({
             deps.yearRacesListDataservice = new deps['mobiquity.core.yearRacesListDataservice']();
         });
 
-        it('Champions dataservice should be loaded', function() {
+        it('Year race list dataservice should be loaded', function() {
             expect(deps.yearRacesListDataservice).toBeDefined();
         });
+
+        it('It should call getYearRacesList - with success', function() {
+            deps.$httpBackend.whenGET(deps.mockedData.regexp.yearsResult).respond(deps.mockYearRaceResults);
+            deps.yearRacesListDataservice.getYearRacesList().then(
+                function(result) {
+                    expect(result).toEqual(deps.mockYearRaceResults.RaceTable.Races);
+                }
+            );
+        });
+
+        it('It should call getYearRacesList - with error 500', function() {
+            deps.$httpBackend.whenGET(deps.mockedData.regexp.yearsResult).respond(500, 'error');
+            deps.yearRacesListDataservice.getYearRacesList().then(
+                deps.mockedData.failedPromise,
+                function(result) {
+                    expect(result).toEqual(deps.mockedData.response.error);
+                }
+            );
+        });
+
+        /*    afterEach(function() {
+         deps.$httpBackend.flush();
+         deps.$rootScope.$digest();
+         //deps.$httpBackend.verifyNoOutstandingExpectation();
+         deps.$httpBackend.verifyNoOutstandingRequest();
+         });*/
     }
 });
